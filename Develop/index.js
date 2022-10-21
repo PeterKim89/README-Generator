a26a45ff862eacf800513aa030379a7bd4ca4f87
Peter Kim <peter.kim@uconn.edu>
2022-10-21 00:23:01 -0400

    List of questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,18 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// Project title, description, installation instructions, usage information, contribution guidelines, test instructions, license, github username, email, 
+const questions = [
+    "What's your project's title?",
+    "Write a short description for your project.",
+    "Write some installation instructions for your project.",
+    "How do you use your project?",
+    "Write some contribution guidelines for others.",
+    "Write some example tests for your project.",
+    "Choose a license.",
+    "What's your GitHub username?",
+    "What's your email?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
