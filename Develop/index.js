7347f84eb566e1accdd5be87a14acaa47b6ea13b
Peter Kim <peter.kim@uconn.edu>
2022-10-21 01:28:10 -0400

    writeToFile

--- Develop/index.js
+++ Develop/index.js
@@ -13,18 +13,20 @@ const questions = [
     "Write some example tests for your project.",
     "What's your GitHub username?",
     "What's your email?",
-    "Choose a license."
+    "Choose a license for your project."
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
-
-    }
+        err ? console.log(err) : console.log("The README has been successfully created!");
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
