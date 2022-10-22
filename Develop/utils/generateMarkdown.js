8ea7a5fb44c6a9f881f90dd321188d4292d5d7b8
Peter Kim <peter.kim@uconn.edu>
2022-10-21 21:21:36 -0400

    gitignore adds test md files, generateMarkdown formatting

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -41,65 +41,24 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   let licenseText = "";
   if (license === "MIT") {
-    licenseText = 
-    `MIT License
-    \n
-    Copyright (c) [year] [fullname]
-    \n
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    \n
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    \n
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.`;
+    licenseText = `## License\n
+    [MIT]${renderLicenseLink("MIT")}
+    `
   }
   else if (license === "APACHE 2.0") {
-    licenseText = 
-    `Copyright [yyyy] [name of copyright owner]
-    \n
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
- 
-      http://www.apache.org/licenses/LICENSE-2.0
- 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.`;
+    licenseText = `## License\n
+    [APACHE 2.0]${renderLicenseLink("APACHE 2.0")}
+    `
   }
   else if (license === "GPL 3.0") {
-    licenseText = `Copyright (C) <year>  <name of author>\n
-    This program comes with ABSOLUTELY NO WARRANTY; for details type "show w".
-    This is free software, and you are welcome to redistribute it
-    under certain conditions; type "show c" for details.`;
+    licenseText = `## License\n
+    [GPL 3.0]${renderLicenseLink("GPL 3.0")}
+    `
   }
   else if (license === "BSD 3") {
-    licenseText = `
-    Copyright <YEAR> <COPYRIGHT HOLDER>
-    \n
-    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-    \n
-    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-    \n
-    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-    \n
-    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-    \n
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        `;
+    licenseText = `## License\n
+    [BSD 3]${renderLicenseLink("BSD 3")}
+    `
   }
   return licenseText;
 }
@@ -108,6 +67,7 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   return `
   # ${data.title}
+  ${renderLicenseBadge(data.license)}
   
   ## Table of Contents
   * [Description][#description]
@@ -134,10 +94,13 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## Author
-  ${data.username}
+  [GitHub](https://www.github.com/${data.username})
+  [Email]:${data.email}
 
   ## Questions
   If you have any other questions, please contact me at ${data.email}.
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
