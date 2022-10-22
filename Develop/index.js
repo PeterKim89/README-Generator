399a76c9daab08c5a653ebe48fbdf14a7520770c
Peter Kim <peter.kim@uconn.edu>
2022-10-21 22:28:22 -0400

    comments throughout

--- Develop/index.js
+++ Develop/index.js
@@ -8,48 +8,61 @@ const generateReadMe = require("./utils/generateMarkdown.js");
 // Project title, description, installation instructions, usage information, contribution guidelines, test instructions, license, github username, email, 
 const questions = () => {
     return inquirer.prompt([
+        // takes in user input for project title
         {
             type: "input",
             name: "title",
             message: "What's your project's title?"
         },  
+        // takes in user input for project descriptions
         {
             type: "input",
             name: "description",
             message: "Write a short description for your project."
         },
+        // takes in user input for installation
+        // defaults to npm i
         {
             type: "input",
             name: "installation",
             message: "What command should be run to install dependencies?",
             default: "npm i"
         },
+        // takes in user input for any project usage information
         {
             type: "input",
             name: "usage",
             message: "What should be known to use your project?"
         },
+        // takes in user input for any project contribution methods
         {
             type: "input",
             name: "contribution",
             message: "What should be known to contribute to your project?",
         },
+        // takes in user input for tests
+        // defaults to npm test
         {  
             type: "input",
             name: "tests",
             message: "What command should be run to run tests?",
             default: "npm test"
         },    
+        // takes in user input for their GitHub username
         {
             type: "input",
             name: "username",
             message: "What's your GitHub username?",
         },
+        // takes in user input for their email
         {
             type: "input",
             name: "email",
             message: "What's your email?",
         },
+        // displays a list of licenses
+        // defaults to MIT license
+        // user can choose 1 using arrow keys
         {
             type: "list",
             name: "license",
@@ -62,17 +75,22 @@ const questions = () => {
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    // function to write the file with passed data
     fs.writeFile(fileName, data, err => {
+        // checks if there's an error, if yes: console logs the error. if no: console logs a success message
         err ? console.log(err) : console.log("The README has been successfully created!");
     });
 }
 
 // TODO: Create a function to initialize app
 function init() {
+    // prompts the array of questions
     questions()
+    // user answers are then passed to the generateReadMe function to fill in user specific data
     .then (answers => {
         return generateReadMe(answers);
     })
+    // writes the actual file using the data
     .then (data => {
         return writeToFile("Generated-README", data);
     })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,18 +2,23 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let badgeLink = "";
+  // if MIT is chosen, returns MIT badge
   if (license === "MIT") {
     badgeLink = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
   }
+  // if APACHE is chosen, returns APACHE badge
   else if (license === "APACHE 2.0") {
     badgeLink = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]";
   }
+  // if GPL is chosen, returns GPL badge
   else if (license === "GPL 3.0") {
     badgeLink = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
   }
+  // if BSD is chosen, returns BSD badge
   else if (license === "BSD 3") {
     badgeLink = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]";
   }
+  // if none chosen, returns an empty string
   return badgeLink;
 }
 
@@ -21,18 +26,22 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   let licenseLink = "";
+  // if MIT is chosen, returns MIT license link
   if (license === "MIT") {
     licenseLink = "(https://opensource.org/licenses/MIT)";
   }
+  // if APACHE is chosen, returns MIT license link
   else if (license === "APACHE 2.0") {
     licenseLink = "(https://opensource.org/licenses/Apache-2.0)";
   }
+  // if GPL is chosen, returns MIT license link
   else if (license === "GPL 3.0") {
     licenseLink = "(https://www.gnu.org/licenses/gpl-3.0)";
-  }
+  }// if BSD is chosen, returns MIT license link
   else if (license === "BSD 3") {
     licenseLink = "(https://opensource.org/licenses/BSD-3-Clause)";
   }
+  // if none chosen, returns an empty string
   return licenseLink;
 }
 
@@ -40,30 +49,36 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   let licenseText = "";
+  // if MIT is chosen, returns MIT license
   if (license === "MIT") {
     licenseText = `## License
     [MIT]${renderLicenseLink("MIT")}
     `
   }
+  // if MIT is chosen, returns APACHE license
   else if (license === "APACHE 2.0") {
     licenseText = `## License\n
     [APACHE 2.0]${renderLicenseLink("APACHE 2.0")}
     `
   }
+  // if MIT is chosen, returns GPL license
   else if (license === "GPL 3.0") {
     licenseText = `## License\n
     [GPL 3.0]${renderLicenseLink("GPL 3.0")}
     `
   }
+  // if MIT is chosen, returns BSD license
   else if (license === "BSD 3") {
     licenseText = `## License\n
     [BSD 3]${renderLicenseLink("BSD 3")}
     `
   }
+  // if none chosen, returns an empty string
   return licenseText;
 }
 
 // TODO: Create a function to generate markdown for README
+// formats how the readme should look and where to put user data
 function generateMarkdown(data) {
   return `
   # ${data.title}
